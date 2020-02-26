85fccfbb56974af3d4c4c3a95117e77c3a985e2d
marcos <marcoswoc@outlook.com>
2020-02-26 13:26:18 -0300

    Criando testes unitarios do TodoHandler e criando Fake Repository para testes.

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Tests.repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da tarefa", "MarcosWOC", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_esxcucao()
+        {             
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {            
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+        }
+    }
+}
